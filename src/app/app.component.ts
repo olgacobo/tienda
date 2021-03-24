71470f3751fe647a36091860d0535fa71197f207
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-24 13:00:15 +0100

    Productos solo salen al poner localhost:4200/products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Product} from './product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -15,51 +15,7 @@ export class AppComponent {
   // creo e inicializo un power para usar la pipe
   power = 10;
 
-  // creo mi array de productos, importante []
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/img/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'Descripcion de la camiseta'
-    },
-    {
-      id: '2',
-      image: 'assets/img/hoodie.png',
-      title: 'Sudadera',
-      price: 80000,
-      description: 'Descripcion de la sudadera '
-    },
-    {
-      id: '3',
-      image: 'assets/img/mug.png',
-      title: 'Taza',
-      price: 60000,
-      description: 'Descripcion de la taza'
-    },
-    {
-      id: '4',
-      image: 'assets/img/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'Descripcion del pin'
-    },
-    {
-      id: '5',
-      image: 'assets/img/stickers1.png',
-      title: 'Stickers',
-      price: 3000,
-      description: 'Descripcion del sticker 1'
-    },
-    {
-      id: '6',
-      image: 'assets/img/stickers2.png',
-      title: 'Stickers',
-      price: 200,
-      description: 'Descripcion del sticker 2'
-    }
-  ];
+ 
 
 
   // creo una funcion metodo para añadir items (todos se llamaran nuevo item)
@@ -72,10 +28,6 @@ export class AppComponent {
     this.items.splice(index, 1); // la posicion que queremos borrar, y cuantos elementos queremos borrar a partir de esa posicion
   }
 
-  // creo una funcion para sacar el output
-  clickProduct(id: number){
-    console.log('product');
-    console.log(id);
-  }
+  
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product} from '../product.model';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+   // creo mi array de productos, importante []
+   products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/img/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'Descripcion de la camiseta'
+    },
+    {
+      id: '2',
+      image: 'assets/img/hoodie.png',
+      title: 'Sudadera',
+      price: 80000,
+      description: 'Descripcion de la sudadera '
+    },
+    {
+      id: '3',
+      image: 'assets/img/mug.png',
+      title: 'Taza',
+      price: 60000,
+      description: 'Descripcion de la taza'
+    },
+    {
+      id: '4',
+      image: 'assets/img/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'Descripcion del pin'
+    },
+    {
+      id: '5',
+      image: 'assets/img/stickers1.png',
+      title: 'Stickers',
+      price: 3000,
+      description: 'Descripcion del sticker 1'
+    },
+    {
+      id: '6',
+      image: 'assets/img/stickers2.png',
+      title: 'Stickers',
+      price: 200,
+      description: 'Descripcion del sticker 2'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // creo una funcion para sacar el output
+  clickProduct(id: number){
+    console.log('product');
+    console.log(id);
   }
 
 }
