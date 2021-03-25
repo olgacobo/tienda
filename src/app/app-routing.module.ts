ad31c861c5256e38f96d555c2f07ca7a8bb7e86d
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-25 11:33:29 +0100

    Modificacion para trabajar con vistas anidadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,46 @@ import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 // importante poner en el app.component.html <router-outlet></router-outlet>
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    // lo madamos a la pagina de home cuando el pathmartch sea full, cunado tengamos la url en seco, sin ningun path
-    pathMatch: 'full'
+    component: LayoutComponent,
+    children: [ // todos estos componentes aplicaran el layout
+      {
+        path: '',
+        redirectTo: '/home',
+        // lo madamos a la pagina de home cuando el pathmartch sea full, cunado tengamos la url en seco, sin ningun path
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
+        path: 'products/:id', // no puede ser solo products porque colisionarian las rutas, va a renderizar el productdetailcomponent
+        component: ProductDetailComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: 'demo',
+        component: DemoComponent
+      },
+      {
+        path: '**', // significa que no hubo match
+        component: PageNotFoundComponent
+      }
+    ]
   },
   {
     path: 'home',
