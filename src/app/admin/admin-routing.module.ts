89957bf8319dab94f3b8fe65045fd7ccbd48260d
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-30 16:35:09 +0200

    Añadida ruta del nav admin para que salga por defecto al poner /admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
     path: 'create',
     component: ProductFormComponent
+  },
+  {
+    path: '',
+    component: NavComponent
   }
 ];
 
