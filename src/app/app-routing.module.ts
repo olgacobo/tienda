022f79bccd4243ccb942a239164cf420b060d639
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-30 14:53:45 +0200

    Arreglando cagadas varias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,12 +36,16 @@ const routes: Routes = [
       {
         path: 'demo',
         loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
-      },
-      {
-        path: '**', // significa que no hubo match
-        loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
-      }
+      }      
     ]
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: '**', // significa que no hubo match
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
   }
 ];
 
