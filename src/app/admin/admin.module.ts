2b905a020cfefaf5e9fb950986fa669de40fe0e2
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-30 16:26:53 +0200

    Genero navigation de admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,15 +7,28 @@ import { ProductFormComponent } from './components/product-form/product-form.com
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../material/material.module';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
-  declarations: [ProductFormComponent],
+  declarations: [ProductFormComponent, NavComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class AdminModule { }
